30cb348048e4c6120a1e1c2c5cefd1e5dacdfe9e
Demo User <debian@beaglebone.localdomain>
2017-10-31 07:02:48 +0000

    Another day, another few dozen lines

--- aREST.cpp
+++ aREST.cpp
@@ -20,28 +20,40 @@
 #include "schema/interface_schema.h"
 
 #include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+#include "rapidjson/schema.h"
+#include <curl/curl.h>
 
 using namespace std;
+using namespace rapidjson;
 
 //---------------------------------------------------------
 // Constructor
 
-aREST::aREST()
-{
+aREST::aREST() {
+
+  isSerial        = false;
+  serialDevice    = "";
+  serialSpeed     = -1;
+  url             = "";
+  digitalPoll     = -1;
+  analogPoll      = -1;
+  variablePoll    = -1;
+
 }
 
 //---------------------------------------------------------
 // Destructor
 
-aREST::~aREST()
-{
+aREST::~aREST() {
+  for (auto a : incomingItems) {delete a;};
+  for (auto a : outgoingItems) {delete a;};
 }
 
 //---------------------------------------------------------
 // Procedure: OnNewMail
 
-bool aREST::OnNewMail(MOOSMSG_LIST &NewMail)
-{
+bool aREST::OnNewMail(MOOSMSG_LIST &NewMail) {
   AppCastingMOOSApp::OnNewMail(NewMail);
 
   MOOSMSG_LIST::iterator p;
@@ -49,22 +61,13 @@ bool aREST::OnNewMail(MOOSMSG_LIST &NewMail)
     CMOOSMsg &msg = *p;
     string key    = msg.GetKey();
 
-#if 0 // Keep these around just for template
-    string comm  = msg.GetCommunity();
-    double dval  = msg.GetDouble();
-    string sval  = msg.GetString(); 
-    string msrc  = msg.GetSource();
-    double mtime = msg.GetTime();
-    bool   mdbl  = msg.IsDouble();
-    bool   mstr  = msg.IsString();
-#endif
+    for (auto a : this->incomingItems) {
+      a->readMail(msg);
+    }
 
-     if(key == "FOO") 
-       cout << "great!";
-
-     else if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
-       reportRunWarning("Unhandled Mail: " + key);
-   }
+    if(key != "APPCAST_REQ") // handled by AppCastingMOOSApp
+      reportRunWarning("Unhandled Mail: " + key);
+    }
 	
    return(true);
 }
@@ -72,8 +75,7 @@ bool aREST::OnNewMail(MOOSMSG_LIST &NewMail)
 //---------------------------------------------------------
 // Procedure: OnConnectToServer
 
-bool aREST::OnConnectToServer()
-{
+bool aREST::OnConnectToServer() {
    registerVariables();
    return(true);
 }
@@ -94,8 +96,7 @@ bool aREST::Iterate()
 // Procedure: OnStartUp()
 //            happens before connection is open
 
-bool aREST::OnStartUp()
-{
+bool aREST::OnStartUp() {
   AppCastingMOOSApp::OnStartUp();
 
   STRING_LIST sParams;
@@ -111,11 +112,21 @@ bool aREST::OnStartUp()
     string value = line;
 
     bool handled = false;
-    if(param == "FOO") {
+    if(param == "INTERFACE") {
+      handled = loadInterface(value);
+    } else if (param == "CONFFILE") {
+      handled = loadConfFile(value);
+    } else if (param == "DIGITALPOLLPERIOD") {
+      digitalPoll = atof(value.c_str());
       handled = true;
-    }
-    else if(param == "BAR") {
+    } else if (param == "ANALOGPOLLPERIOD") {
+      analogPoll = atof(value.c_str());
       handled = true;
+    } else if (param == "VARIABLEPOLLPERIOD") {
+      variablePoll = atof(value.c_str());
+      handled = true;
+    } else {
+      handled = loadItem(param, value);
     }
 
     if(!handled)
@@ -130,8 +141,10 @@ bool aREST::OnStartUp()
 //---------------------------------------------------------
 // Procedure: registerVariables
 
-void aREST::registerVariables()
-{
+void aREST::registerVariables() {
+  for (auto a : this->incomingItems) {
+    a->registerVars();
+  }
   AppCastingMOOSApp::RegisterVariables();
   // Register("FOOBAR", 0);
 }
@@ -155,6 +168,52 @@ bool aREST::buildReport()
   return(true);
 }
 
+//------------------------------------------------------------
+// Procedure: loadInterface()
+
+bool aREST::loadInterface(std::string value) {
+  string schema_str;
+  Document interface, sd;
+
+  if (interface.Parse(value.c_str()).HasParseError()) {
+    return false;
+  }
+  schema_str.assign(reinterpret_cast<const char*>(interface_schema_json), 
+                    interface_schema_json_len);
+  if (sd.Parse(schema_str.c_str()).HasParseError()) {
+    return false;
+  }
+  SchemaDocument schema(sd);
+  SchemaValidator validator(schema);
+  if (!interface.Accept(validator)) {
+    return false;
+  }
+
+  
+
+  return false;
+}
+
+bool aREST::loadInterface(rapidjson::Document& d) {
+
+  return false;
+}
+
+//------------------------------------------------------------
+// Procedure: loadConfFile()
+
+bool aREST::loadConfFile(std::string value) {
+
+  return false;
+}
+
+//------------------------------------------------------------
+// Procedure: loadItem()
+
+bool aREST::loadItem(std::string param, std::string value) {
+
+  return false;
+}
 
 
 
--- aREST.h
+++ aREST.h
@@ -10,18 +10,114 @@
 
 #include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
 #include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+#include <cstdarg>
 
 class aRESTItem {
 	public:
-		virtual aRESTItem(rapidjson::Document config);
-		~aRESTItem();
+		aRESTItem() {newMail = false;};
+		~aRESTItem() {};
 
-		virtual bool fetchItem();
+		static aRESTItem* itemFactory(rapidjson::Document& d);
+
+		virtual bool fetchItem() = 0;
+		virtual bool subscribeVariable(std::string var, ...) = 0;
+		virtual bool readMail(CMOOSMsg &msg) = 0;
+		virtual bool registerVars() = 0;
 
 	protected:
+		std::string fetchSerial(std::string request);
+		std::string fetchURL(std::string request);
+		bool newMail;
+};
 
+class DigitalRead : public aRESTItem {
+	public:
+		DigitalRead(rapidjson::Document& d);
+		~DigitalRead();
+		bool fetchItem();
+		bool subscribeVariable(std::string var, ...) {return false;};
+		bool readMail(CMOOSMsg &msg) {return false;};
+		bool registerVars() {return false;};
 
-}
+	protected:
+		std::string varName;
+		bool state;
+};
+
+class DigitalWrite : public aRESTItem {
+	public:
+		DigitalWrite(rapidjson::Document& d);
+		~DigitalWrite();
+		bool fetchItem();
+		bool subscribeVariable(std::string var, ...);
+		bool readMail(CMOOSMsg &msg);
+		bool registerVars();
+
+	protected:
+		std::string varName;
+		bool state;
+};
+
+class AnalogRead : public aRESTItem {
+	public:
+		AnalogRead(rapidjson::Document& d);
+		~AnalogRead();
+		bool fetchItem();
+		bool subscribeVariable(std::string var, ...) {return false;};
+		bool readMail(CMOOSMsg &msg) {return false;};
+		bool registerVars() {return false;};
+
+	protected:
+		uint16_t state;
+};
+
+class AnalogWrite : public aRESTItem {
+	public:
+		AnalogWrite(rapidjson::Document& d);
+		~AnalogWrite();
+		bool fetchItem();
+		bool subscribeVariable(std::string var, ...);
+		bool readMail(CMOOSMsg &msg);
+		bool registerVars();
+
+	protected:
+		std::string varName;
+		uint16_t state;
+};
+
+class Variable : public aRESTItem {
+	public:
+		Variable(rapidjson::Document& d);
+		~Variable();
+		bool fetchItem();
+		bool subscribeVariable(std::string var, ...) {return false;};
+		bool readMail(CMOOSMsg &msg) {return false;};
+		bool registerVars() {return false;};
+
+	protected:
+		bool isString;
+		bool isDouble;
+		std::string stringResult;
+		double doubleResult;
+};
+
+class Function : public aRESTItem {
+	public:
+		Function(rapidjson::Document& d);
+		~Function();
+		bool fetchItem();
+		bool subscribeVariable(std::string var, ...);
+		bool readMail(CMOOSMsg &msg);
+		bool registerVars();
+
+	protected:
+		std::vector<std::string> vars;
+		bool isString;
+		bool isDouble;
+		std::string stringResult;
+		double doubleResult;
+};
 
 class aREST : public AppCastingMOOSApp
 {
@@ -34,6 +130,10 @@ class aREST : public AppCastingMOOSApp
    bool Iterate();
    bool OnConnectToServer();
    bool OnStartUp();
+   bool loadInterface(std::string value);
+   bool loadInterface(rapidjson::Document& d);
+   bool loadConfFile(std::string value);
+   bool loadItem(std::string param, std::string value);
 
  protected: // Standard AppCastingMOOSApp function to overload 
    bool buildReport();
@@ -48,8 +148,14 @@ class aREST : public AppCastingMOOSApp
  	std::string					serialDevice;
  	int							serialSpeed;
  	std::string					url;
+ 	int 						digitalPoll;
+ 	int 						analogPoll;
+ 	int 						variablePoll;
 
  private: // State variables
+ 	int 						lastDigitalPoll;
+ 	int 						lastAnalogPoll;
+ 	int 						lastVariablePoll;
 };
 
 #endif 
